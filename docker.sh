3fed7d35cdacf7fe98116b34679ce170b6b8268f
Luis Barron <luis@barron.im>
2019-12-19 10:48:58 -0500

    Docker

--- docker.sh
+++ docker.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 docker rm -f static-site
 docker build . -t simple-nginx
-docker run --rm -itd -p 80:80 simple-nginx --name static-site
+docker run --rm -itd -p 80:80 --name static-site simple-nginx
